af83ed8b1e3c14f1f0459dbb8639acc2ebf9cb16
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2024-11-05 21:22:58 +0530

    Add DB connection

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from db import conn
 
 app = Flask(__name__)
 
---
+++ db.py
@@ -0,0 +1,11 @@
+from sqlite3 import connect, Row
+
+conn = connect("frappe.db")
+
+# Foreign keys have to be turned on manually in sqlite3 as it turns out
+conn.execute("PRAGMA foreign_keys = ON")
+
+# Return query output as dict key_value pairs instead of a confusing tuple
+conn.row_factory = Row
+
+conn.commit()
