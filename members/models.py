6c52ec1f334d2a03a7de49b330b94068ffd552dc
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2024-11-05 23:04:20 +0530

    Create create method for members

--- members/models.py
+++ members/models.py
@@ -1,7 +1,23 @@
 from ..db import conn
+
+
 def read():
     cur = conn.cursor()
     cur.execute("select memberid, name, registration_timestamp from members")
     members = cur.fetchall()
     cur.close()
     return members
+
+
+def create(name):
+    cur = conn.cursor()
+    try:
+        cur.execute("insert into members(name) values (?)", (name,))
+        id = cur.lastrowid
+        print(id)
+        cur.close()
+        conn.commit()
+    except Exception as e:
+        print(e)
+        return False
+    return True
--- members/routes.py
+++ members/routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from .models import read
+from flask import Blueprint, render_template, request
+from .models import read, create
 
 route = Blueprint("members", __name__, url_prefix="/members")
 
@@ -8,3 +8,20 @@ route = Blueprint("members", __name__, url_prefix="/members")
 def view():
     members = read()
     return render_template("members/index.html", members=members)
+
+
+@route.route("/create", methods=["GET", "POST"])
+def create_view():
+    if request.method == "GET":
+        members = read()
+        return render_template("members/create.html", members=members)
+    if request.method == "POST":
+        name = request.form["name"]
+        if create(name):
+            return render_template(
+                "success.html", message=f"Successfully created member {name}!"
+            )
+        return render_template(
+            "failure.html",
+            message=f"There was a problem creating the member {name}!",
+        )
