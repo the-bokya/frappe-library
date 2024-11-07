98ff1bc41c589b215c574b9ac92a1bef9cf14d5a
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2024-11-07 19:53:26 +0530

    Format queries

--- setup.py
+++ setup.py
@@ -5,16 +5,44 @@ conn = connect("frappe.db")
 # Initialise members
 # registration time is set through current_timestamp (this should be an editable property I guess in the CRUD view).
 conn.execute(
-    "create table if not exists members (memberid integer primary key autoincrement, name text, registration_timestamp datetime default current_timestamp)"
+    """
+    create table if not exists members
+    (
+        memberid integer primary key autoincrement,
+        name text,
+        registration_timestamp datetime default current_timestamp
+    )
+    """
 )
 
 # Initialise books
 conn.execute(
-    "create table if not exists books (bookid integer primary key, title text, authors text, isbn text,language_code text, publication_date date, publisher text, amount integer)"
+    """create table if not exists books
+    (
+        bookid integer primary key,
+        title text,
+        authors text,
+        isbn text,
+        language_code text,
+        publication_date date,
+        publisher text,
+        amount integer
+    )
+    """
 )
 # Initialise transactions
 conn.execute(
-    "create table if not exists transactions(bookid integer, memberid integer, issue_date date default (date('now')), return_date date, returned boolean default 0, foreign key(bookid) references books(bookid), foreign key (memberid) references members(memberid))"
+    """create table if not exists transactions
+    (
+        bookid integer,
+        memberid integer,
+        issue_date date default (date('now')),
+        return_date date,
+        returned boolean default 0,
+        foreign key(bookid) references books(bookid),
+        foreign key (memberid) references members(memberid)
+    )
+    """
 )
 
 # The view that gets member info alongwith pending amount and issued books. This will be the default view from now on ig
